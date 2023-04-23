c2312256ec175264970f4172f34939450d1c2a88
David L. Inman <davidlevere@gmail.com>
2023-04-22 23:04:24 -0400

    move def stack impl

--- src/data.rs
+++ src/data.rs
@@ -37,3 +37,40 @@ pub struct ExeEnv {
     pub data_stack : Vec<IlData>,
     pub def_stack : Vec<HashMap<String, IlData>>,
 }
+
+pub struct DefStack<'a> {
+    base : &'a mut HashMap<String, IlData>,
+    stack : Vec<HashMap<String, IlData>>,
+}
+
+impl<'a> DefStack<'a> {
+    fn new(base : &'a mut HashMap<String, IlData>) -> Self {
+        DefStack { base, stack : vec![] }
+    }
+
+    fn get(&self, name : &String) -> Option<&IlData> {
+        let target = self.stack.iter().rev().find(|map| map.contains_key(name));
+        match target { 
+            Some(map) => map.get(name),
+            None => self.base.get(name),
+        }
+    } 
+
+    fn set(&mut self, name : String, data : IlData) {
+        if self.stack.len() > 0 {
+            let last = self.stack.len() - 1;
+            self.stack[last].insert(name, data);
+        }
+        else {
+            self.base.insert(name, data);
+        }
+    }
+
+    fn push(&mut self) {
+        self.stack.push(HashMap::new());
+    }
+
+    fn pop(&mut self) {
+        self.stack.pop();
+    }
+}
