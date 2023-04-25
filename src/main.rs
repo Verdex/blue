59d1da2b65e198acd652e91b50c2befc69c37d2a
David L. Inman <davidlevere@gmail.com>
2023-04-24 22:42:46 -0400

    test

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,24 @@ mod data;
 mod machine;
 
 fn main() {
-    println!("Hello, world!");
+
+    use crate::data::*;
+
+    use std::collections::HashMap;
+    use std::rc::Rc;
+
+    let mut defs = HashMap::new();
+    let mut dict = HashMap::new();
+
+    let blarg = Rc::new(Word::Il(vec![]));
+    dict.insert("blarg".into(), blarg.clone());
+
+    let other = Rc::new(Word::Func(vec![blarg.clone(), blarg.clone()]));
+
+    let main = Rc::new(Word::Func(vec![other.clone(), other.clone()]));
+    dict.insert("main".into(), main);
+
+
+
+    machine::execute("main".into(), &mut dict, &mut defs);
 }
